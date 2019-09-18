d02edd69461a16883a1a0309a450df25e7219741
Emrullah Rexhepi <em.rexhepi@gmail.com>
2019-09-18 19:15:09 +0200

    Brand New Start

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// require libraries
-const Transcorder = require('./Transcorder/Transcorder');
-
-// require database
-const db = require('./databases/db');
-
-// declare transcorder
-const transcorder = new Transcorder(db);
